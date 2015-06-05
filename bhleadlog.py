71dfa85324e13f19e24c319d250e39e95c928482
eckardm <eckardm@umich.edu>
2015-06-05 13:07:39 -0400

    dictionary for eadids and titlepropers, also tweaked text for them

--- bhleadlog.py
+++ bhleadlog.py
@@ -1,6 +1,60 @@
 # import what we need
+import os
+from os.path import join
+from lxml import etree
 import csv
 import re
+
+'''
+first, let's create a dictionary of ead identifiers and their corresponding title propers'''
+
+# empty dictionary for ead identifier keys and title proper values
+eadids_and_titlepropers = {}
+
+# where are the eads?
+ead_path = 'S:/Curation/Projects/Mellon/ArchivesSpace/ATeam_Migration/EADs/Real_Masters_all'
+
+# make sure we only get the xml
+eads = re.compile('\.xml$')
+
+# xpaths we'll need
+eadid_xpath = '//eadid'
+titleproper_xpath = '//titlestmt/titleproper'
+
+# print that we're creating the dictionary
+print 'Creating dictionary.\n'
+
+# go through the eads
+for filename in os.listdir(ead_path):
+    if eads.search(filename):
+        # let us know we're still working on it
+        print '\rWorking on it... |',
+        print '\rWorking on it... /',
+        print '\rWorking on it... -',
+        print '\rWorking on it... \\',
+        print '\rWorking on it... |',
+        print '\rWorking on it... /',
+        print '\rWorking on it... -',
+        print '\rWorking on it... -',
+        print '\rWorking on it... \\',
+        # parse the ead
+        ead_tree = etree.parse(join(ead_path, filename))
+        # get the eadid xml entity
+        eadid = ead_tree.xpath(eadid_xpath)
+        # get the titleproper xml entity
+        titleproper = ead_tree.xpath(titleproper_xpath)
+        # get the eadid text
+        eadid_text = eadid[0].text
+        # get the titleproper text, and get the line breaks and encoding right
+        titleproper_text = titleproper[0].text.replace('\n', '').encode('ascii', 'ignore')
+        # add both do the ead identifier and title proper dictionary
+        eadids_and_titlepropers[eadid_text] = titleproper_text
+
+# print that dictionary got created
+print 'Dictionary created.\n'
+
+'''
+second, lets get the unique ead identifiers and queries from the logs, and use the dictionary to tell us what the corresponding title proper is for each ead identifier'''
 
 # empty lists for ead identifiers and queries
 ead_identifiers = []
@@ -10,9 +64,13 @@ queries = []
 requests_count = 0
 ead_identifiers_count = 0
 queries_count = 0
+key_error_counter = 0
 
 # where are the logfiles?
-logfiles = 'path/to/subsetbhleadlog.csv'
+logfiles = 'subsetbhleadlog.csv'
+
+# print that we're starting the logs
+print 'Going through logs.\n'
 
 # open the logs
 with open(logfiles, 'rb') as csvfile:
@@ -25,7 +83,7 @@ with open(logfiles, 'rb') as csvfile:
         # update count
         requests_count += 1
         # find the ead identifiers
-        ead_identifier_matches = re.findall('umich-bhl-\d+\.?\d', request)
+        ead_identifier_matches = re.findall('umich-bhl-\d+\.?-?\d', request)
         # go through them
         for ead_identifier in ead_identifier_matches:
             # make sure they are unique
@@ -45,18 +103,32 @@ with open(logfiles, 'rb') as csvfile:
                 # update count
                 queries_count += 1
                 # add them to empty lis
+
+# print that we're done with logs
+print 'Done with logs.\n'                
                 
 # print requests
-print 'There were ' + str(requests_count) + ' requests today.'
+print 'REQUESTS\n'
+print 'There were ' + str(requests_count) + ' requests today.\n'
 
-# print unique ead identifiers
-print 'There were ' + str(ead_identifiers_count) + ' unique EADs accessed today.'
-print 'Here they are:'
+# print unique ead identifiers and corresponding title
+print 'EAD IDENTIFIERS\n'
+print 'There were ' + str(ead_identifiers_count) + ' unique EADs accessed today.\n'
+print 'Here they are, with their title propers:\n'
 for i in ead_identifiers:
-    print i
-    
-#print unique requests
-print 'There were ' + str(queries_count) + ' unique queries today.'
-print 'Here they are:'
+    try:
+        print i + '\n' + eadids_and_titlepropers[i] + '\n'
+    except:
+        print 'KEY ERROR on ' + i + '\n'
+        # add key to dictionary with dummy value for future investigation
+        eadids_and_titlepropers[i] = 'UNIDENTIFIED TITLEPROPER'
+        # update count
+        key_error_counter += 1
+print 'There were ' + str(key_error_counter) + ' KEY ERROR(S) found.\n'
+        
+# print unique queries
+print 'QUERIES\n'
+print 'There were ' + str(queries_count) + ' unique queries today.\n'
+print 'Here they are:\n'
 for i in queries:
-    print i
+    print i + '\n'
