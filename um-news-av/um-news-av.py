ae49ed8853fb4ec657b7db84c1c07262c879c82f
eckardm <eckardm@umich.edu>
2015-06-10 15:50:38 -0400

    new project for um news av

---
+++ um-news-av/um-news-av.py
@@ -0,0 +1,18 @@
+'''
+Here's the draft of the FA attached as well as the original metadata spreadsheets I gave Rob. The series that need to be addressed are:
+
+  * Sound Materials - Audio Reel-to-Reel News Briefs (1975-1994)
+  * Audio Podcasts (2006 - 2010)
+  * Digital Video
+
+For the podcasts and video, we want to keep:
+
+  * Unit title
+  * Unit date
+  * Note field
+  * Physical facet--file extension(s)
+
+Let me know if you have any questions--thanks!
+
+MIke'''
+
