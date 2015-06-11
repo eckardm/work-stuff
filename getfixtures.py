20b5fa7a252bff45cc04f5ae789c867f5f2210cf
eckardm <eckardm@umich.edu>
2015-06-11 15:30:14 -0400

    removing fixtures

--- getfixtures.py
+++
@@ -1,8 +0,0 @@
-# import what we need
-import xmltodict
-import json
-
-xml = 'S:/MLibrary/archivematica/backlog/originals/max-0713_0001-251d3e4d-88c5-45b2-ab17-5d058247cffc/metadata/submissionDocumentation/METS.xml'
-
-o = xmltodict.parse(xml)
-json.dumps(o)
