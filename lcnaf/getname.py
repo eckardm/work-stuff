f882e9a28ceb1203fbea2814d585e5200af0ef7b
eckardm <eckardm@umich.edu>
2015-06-29 14:08:23 -0400

    got the spacing right and simplified the search for --

--- lcnaf/getname.py
+++ lcnaf/getname.py
@@ -30,28 +30,19 @@ for filename in os.listdir(ead_path):
     if xml.search(filename):
         ead_tree = ET.parse(join(ead_path, filename))
         for sub in ead_tree.xpath(controlaccess_xpath):
-            try:
-                if sub.tag == 'persname' and sub.text is not None:
-                    if '--' in sub.text:
-                        compound_subject_matches = re.findall('(.*?)(?=--)', sub.text)
-                        for compound_subject_match in compound_subject_matches:
-                            output = compound_subject_match
-                    else:
-                        output = sub.text
-                    with open(persname_output, 'a') as text_file:
-                        text_file.write(output.encode("utf-8") + '\n')
-                elif sub.tag == 'corpname' and sub.text is not None:
-                    if '--' in sub.text:
-                        compound_subject_matches = re.findall('(.*?)(?=--)', sub.text)
-                        for compound_subject_match in compound_subject_matches:
-                            output = compound_subject_match
-                    else:
-                        output = sub.text
-                    with open(corpname_output, 'a') as text_file:
-                        text_file.write(output.encode("utf-8") + '\n')
-            except:
-                error_counter += 1
-                with open(error_output, 'a') as text_file:
-                    text_file.write(filename + ', ' + sub.tag + '\n')
+            if sub.tag == 'persname' and sub.text is not None:
+                if '--' in sub.text:
+                    output = sub.text.split('--')[0].strip()
+                else:
+                    output = sub.text
+                with open(persname_output, 'a') as text_file:
+                    text_file.write(output.encode("utf-8") + '\n')
+            elif sub.tag == 'corpname' and sub.text is not None:
+                if '--' in sub.text:
+                    output = sub.text.split('--')[0].strip()
+                else:
+                    output = sub.text
+                with open(corpname_output, 'a') as text_file:
+                    text_file.write(output.encode("utf-8") + '\n')
                     
 print 'There were ' + str(error_counter) + ' errors!'
