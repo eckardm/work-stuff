6c18bd00d80a41b2dbe1ce58e77d568e551a41a8
eckardm <eckardm@umich.edu>
2015-06-29 13:56:11 -0400

    finally got this right

--- lcnaf/getname.py
+++ lcnaf/getname.py
@@ -1,3 +1,6 @@
+'''
+step one of https://github.com/mcarruthers/LCNAF-Named-Entity-Reconciliation'''
+
 import lxml
 from lxml import etree as ET
 import os
@@ -27,25 +30,25 @@ for filename in os.listdir(ead_path):
     if xml.search(filename):
         ead_tree = ET.parse(join(ead_path, filename))
         for sub in ead_tree.xpath(controlaccess_xpath):
-            try: 
+            try:
                 if sub.tag == 'persname' and sub.text is not None:
                     if '--' in sub.text:
-                        compound_subject_matches = re.findall('(^.*?)(?=--)')
+                        compound_subject_matches = re.findall('(.*?)(?=--)', sub.text)
                         for compound_subject_match in compound_subject_matches:
-                            output = compound_subject_match.encode("utf-8")
+                            output = compound_subject_match
                     else:
-                        output = sub.text.encode("utf-8")
+                        output = sub.text
                     with open(persname_output, 'a') as text_file:
-                        text_file.write(output + '\n')
-                if sub.tag == 'corpname' and sub.text is not None:
+                        text_file.write(output.encode("utf-8") + '\n')
+                elif sub.tag == 'corpname' and sub.text is not None:
                     if '--' in sub.text:
-                        compound_subject_matches = re.findall('(^.*?)(?=--)')
+                        compound_subject_matches = re.findall('(.*?)(?=--)', sub.text)
                         for compound_subject_match in compound_subject_matches:
-                            output = compound_subject_match.encode("utf-8")
+                            output = compound_subject_match
                     else:
-                        output = sub.text.encode("utf-8")
+                        output = sub.text
                     with open(corpname_output, 'a') as text_file:
-                        text_file.write(output + '\n')
+                        text_file.write(output.encode("utf-8") + '\n')
             except:
                 error_counter += 1
                 with open(error_output, 'a') as text_file:
