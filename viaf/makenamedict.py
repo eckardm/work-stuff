88f41f164e77e048d0e78641fd1f7c18257e6a2d
eckardm <eckardm@umich.edu>
2015-07-08 16:06:38 -0400

    added fuzzywuzzy and argument to compare originals with authorities before creating dictionary

--- viaf/makenamedict.py
+++ viaf/makenamedict.py
@@ -1,15 +1,16 @@
 # import what we need
 import csv
+from fuzzywuzzy import fuzz
 
 csv.field_size_limit(1000000000)
 
 # what's coming form openrefine?
 openrefine_persname = 'openrefine_persname.csv'
-# openrefine_corpname = 'openrefine_corpname.csv'
+openrefine_corpname = 'openrefine_corpname.csv'
 
 # empty dictionaries
 persnames_dictionary = {}
-# corpnames_dictionary = {}
+corpnames_dictionary = {}
 
 # print 'Creating persname dictionary.'
 
@@ -30,36 +31,38 @@ with open(openrefine_persname, 'rb') as persnames:
         print '\rWorking on it... -',
         print '\rWorking on it... \\',
         original = row[0]
+        authority = row[1]
         link = row[2]
-        if link is not None and link.startswith('http://id.loc.gov/authorities/names/'):
+        if link is not None and link.startswith('http://id.loc.gov/authorities/names/') and if fuzz.ratio(original, authority) >= 80:
             persnames_dictionary[original] = link
             
 print '\rPersname dictionary created.'
 
-# print 'Creating corpname dictionary.'
+print 'Creating corpname dictionary.'
 
-# # corpnames
-# with open(openrefine_corpname, 'rb') as corpnames:
-    # openrefine_corpname_reader = csv.reader(corpnames)
-    # # skip the first row
-    # next(openrefine_corpname_reader, None)
-    # # make dictionary
-    # for row in openrefine_corpname_reader:
-        # print '\rWorking on it... |',
-        # print '\rWorking on it... /',
-        # print '\rWorking on it... -',
-        # print '\rWorking on it... \\',
-        # print '\rWorking on it... |',
-        # print '\rWorking on it... /',
-        # print '\rWorking on it... -',
-        # print '\rWorking on it... -',
-        # print '\rWorking on it... \\',
-        # original = row[0]
-        # link = row[2]
-        # if link is not None and link.startswith('http://id.loc.gov/authorities/names/'):
-            # corpnames_dictionary[original] = link
+# corpnames
+with open(openrefine_corpname, 'rb') as corpnames:
+    openrefine_corpname_reader = csv.reader(corpnames)
+    # skip the first row
+    next(openrefine_corpname_reader, None)
+    # make dictionary
+    for row in openrefine_corpname_reader:
+        print '\rWorking on it... |',
+        print '\rWorking on it... /',
+        print '\rWorking on it... -',
+        print '\rWorking on it... \\',
+        print '\rWorking on it... |',
+        print '\rWorking on it... /',
+        print '\rWorking on it... -',
+        print '\rWorking on it... -',
+        print '\rWorking on it... \\',
+        original = row[0]
+        authority = row[1]
+        link = row[2]
+        if link is not None and link.startswith('http://id.loc.gov/authorities/names/') and if fuzz.ratio(original, authroity) >= 80:
+            corpnames_dictionary[original] = link
 
-# print '\rCorpname dictionary created.'
+print '\rCorpname dictionary created.'
 
 print 'Writing to constants.'
             
@@ -69,6 +72,6 @@ constants = 'constants.py'
 # put the dictionaries in constants.py
 with open(constants, "a") as txt_file:
     txt_file.write('# dictionary for persnames\npersnames_dictionary = ' + str(persnames_dictionary))
-    # txt_file.write('\n\n# dictionary for corpnames\ncorpnames_dictionary = ' + str(corpnames_dictionary))
+    txt_file.write('\n\n# dictionary for corpnames\ncorpnames_dictionary = ' + str(corpnames_dictionary))
     
 print 'Written to constants.'
