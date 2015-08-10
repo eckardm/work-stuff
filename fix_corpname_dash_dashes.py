2204221d216688fd08325e22be99d171a115791a
eckardm <eckardm@umich.edu>
2015-08-10 16:00:16 -0400

    import what we need

---
+++ fix_corpname_dash_dashes.py
@@ -0,0 +1,7 @@
+'''
+first things first, import what we need'''
+
+# lxml is the most feature-rich and easy-to-use library for processing xml (and html), you'll need to install it
+from lxml import etree
+# os provides a portable way of using operating system dependent functionality
+from os import path
