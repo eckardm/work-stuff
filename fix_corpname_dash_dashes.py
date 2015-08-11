85f745b9f9d549622dd11ec9be39c22921b637f7
eckardm <eckardm@umich.edu>
2015-08-11 10:13:20 -0400

    last thing before adding a check

--- fix_corpname_dash_dashes.py
+++ fix_corpname_dash_dashes.py
@@ -18,124 +18,89 @@ preliminaries'''
 ead_path = 'C:\Users\Public\Documents\Real_Masters_all'
 # ead_path = 'C:\Users\eckardm\GitHub\vandura\Real_Masters_all'
 
-# where are we looking in the eads?
-xpaths = ['//controlaccess', '//origination']
-
 
 '''
 go through the files, find corpnames with dash dashes and parse them out'''
 
-def dash_dashes(xpath):
+# go through the files
+for filename in tqdm(os.listdir(ead_path)):
+    # only look at the xml files
+    if filename.endswith('.xml'):
 
-    # go through the files
-    for filename in tqdm(os.listdir(ead_path)):
-        # only look at the xml files
-        if filename.endswith('.xml'):
-    
-            # create lxml version of the ead
-            ead_tree = etree.parse(path.join(ead_path, filename))
-        
-            # empty lists
+        # create lxml version of the ead
+        ead_tree = etree.parse(path.join(ead_path, filename))
+
+        # only look at controlaccess
+        for controlaccess in ead_tree.xpath('//controlaccess/controlaccess'):
+            
+            # empty lists that we'll use to check for duplicates
             corporate_entities = []
-            subjects = []           
-            for block in ead_tree.xpath(xpath):
-                corporate_entities = []
-                subjects = []
-                block_path = ead_tree.getpath(block)
-                corpnames = block.xpath('.//corpname')
-                # go through each of the corpnames
-                for corpname in corpnames:
-                    if '--' in corpname.text:
-                        # split on the dash dash to get the two parts and add to lists...
-                        # corpnames
-                        # parse
-                        corporate_entity = corpname.text.split('--', 1)[0] + '.'
-                         # add to list if not a duplicate
-                        if corporate_entity not in corporate_entities:
-                            corporate_entities.append(corporate_entity)
-                        # subjects
-                        # parse
-                        subject = xpath_itervar.text.split('--', 1)[1]
-                        # add to list if not a duplicate 
-                        if subject not in subjects:
-                            subjects.append(subject)
-                        
-                        corp_path = ead_tree.getpath(corpname)
-                        block.remove(corp_path)
-                        
-                '''
-                add them as new elements'''
+            subjects = []
+            
+            # getting a list of corpnames in controlaccess, relative to our current position
+            corpnames = controlaccess.xpath('./corpname')
+            
+            # go through each of the corpnames
+            for corpname in corpnames:
+                # find the dash dashes
+                if '--' in corpname.text:
                 
-                # corpnames
-                # go through the list
-                for corpname_itervar in corporate_entities:
-                    # initalize and add tag name
-                    new_corporate_entity = lxml.etree.Element('corpname')
-                    # source
-                    new_corporate_entity.attrib['source'] = 'lcnaf'
-                    # marc field equivalent
-                    new_corporate_entity.attrib['encodinganalog'] = '610'
-                    # text
-                    new_corporate_entity.text = corpname_itervar
-                    # add it
-                    block.append(new_corporate_entity)
-                        
-                # subjects
-                for subject_itervar in subjects:
-                    # initalize and add tag name
-                    new_subject = lxml.etree.Element('subject')
-                    # source
-                    new_subject.attrib['source'] = 'lcsh'
-                    # marc field equivalent
-                    new_subject.attrib['encodinganalog'] = '650'
-                    # text
-                    new_subject.text = subject_itervar
-                    # add it 
-                    block.append(new_subject)
-                    
-               '''
-            write it!'''
-            
-            # setup the writer
-            with open(os.path.join(ead_path, filename), mode="w") as behold_i_am_making_all_things_new:
-                # write
-                behold_i_am_making_all_things_new.write(etree.tostring(ead_tree, xml_declaration=True, encoding='utf-8', pretty_print=True))
-
-'''               
-            
-            for xpath_itervar in ead_tree.xpath(xpath):
-            
-                # find the parent (we'll need it later)
-                parent = xpath_itervar.getparent()
-        
-                # only look at corpnamess and dash dashes
-                if xpath_itervar.tag == 'corpname' and '--' in xpath_itervar.text:
-                    
-                    
-                    corporate_entity = xpath_itervar.text.split('--', 1)[0] + '.'
-                    # add to list if not a duplicate
+                    # split on the dash dash to get the two parts and add to lists...
+                    # corpnames
+                    # parse
+                    corporate_entity = corpname.text.split('--', 1)[0] + '.'
+                     # add to list if not a duplicate
                     if corporate_entity not in corporate_entities:
                         corporate_entities.append(corporate_entity)
-                    
+                        
+                    # subjects
+                    # parse
+                    subject = corpname.text.split('--', 1)[1]
+                    # add to list if not a duplicate 
+                    if subject not in subjects:
+                        subjects.append(subject)
                     
                     
                     '''
+                    delete!'''
                     
+                    # removes child from parent controlaccess
+                    controlaccess.remove(corpname)
                     
+            '''
+            add them as new elements'''
+            
+            # corpnames
+            # go through the list
+            for corpname_itervar in corporate_entities:
+                # initalize and add tag name
+                new_corporate_entity = lxml.etree.Element('corpname')
+                # source
+                new_corporate_entity.attrib['source'] = 'lcnaf'
+                # marc field equivalent
+                new_corporate_entity.attrib['encodinganalog'] = '610'
+                # text
+                new_corporate_entity.text = corpname_itervar
+                # add it
+                controlaccess.append(new_corporate_entity)
                     
-                    '''
-                    deletes the original element'''
-                    # # delete the child
-                    # parent.remove(xpath_itervar)
-                   
-                    
-'''
+            # subjects
+            for subject_itervar in subjects:
+                # initalize and add tag name
+                new_subject = lxml.etree.Element('subject')
+                # source
+                new_subject.attrib['source'] = 'lcsh'
+                # marc field equivalent
+                new_subject.attrib['encodinganalog'] = '650'
+                # text
+                new_subject.text = subject_itervar
+                # add it 
+                controlaccess.append(new_subject)
+                
+        '''
+        write it!'''
         
-     
-                    
-                    
-
-run it!'''
-
-for xpath in xpaths:
-    dash_dashes(xpath)
+        # setup the writer
+        with open(os.path.join(ead_path, filename), mode="w") as behold_i_am_making_all_things_new:
+            # write
+            behold_i_am_making_all_things_new.write(etree.tostring(ead_tree, xml_declaration=True, encoding='utf-8', pretty_print=True))
