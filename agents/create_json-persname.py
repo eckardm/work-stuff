a9aa41999d7c300b7e90ea9e76eb9bf209fedfe2
eckardm <eckardm@umich.edu>
2015-08-17 15:28:33 -0400

    first try at writing json

--- agents/create_json-persname.py
+++ agents/create_json-persname.py
@@ -25,3 +25,15 @@ indicate csv fieldnames for json'''
 field_names = ('prefix', 'title', 'primary_name', 'rest_of_name', 'number', 'suffix', 'fuller_form', 'dates', 'authority_id', 'source', 'name_order')
 
 
+'''
+go through csv and create json'''
+
+# read the csv as if it were a dictionary
+persname_csv_reader = csv.DictReader(persname_csv, field_names)
+
+# go through each row
+for row in persname_csv_reader:
+    # dump the json
+    json.dump(row, persname_json)
+    # add a newline
+    persname_json.write('\n')
