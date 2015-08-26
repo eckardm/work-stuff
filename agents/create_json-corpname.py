24f0e257dbb779c9f7837b7f3443ba441872aed0
Max Eckard <eckardm@umich.edu>
2015-08-26 09:38:47 -0400

    preliminaries

--- agents/create_json-corpname.py
+++ agents/create_json-corpname.py
@@ -7,4 +7,8 @@ import csv
 # json is a lightweight data interchange format inspired by javascript object literal syntax
 import json
 
+'''
+preliminaries'''
 
+# where is the csv that has been exported from openrefine?
+corpname_csv = 'agents-corpname.csv'
