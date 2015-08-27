61678035785860dd4f8d00c6eae2fbd69603c717
Max Eckard <eckardm@umich.edu>
2015-08-27 11:42:51 -0400

    got it working

--- numbers_that_might_be_dates.py
+++ numbers_that_might_be_dates.py
@@ -1,7 +1,7 @@
 '''
 first, import what we need'''
 
-# lxml is a powerful xml document parser, you'll need to download it
+# lxml is a powerful xml document parser, you'll need to install it
 from lxml import etree
 
 # os provides a portable way of using operating system dependent functionality
@@ -16,6 +16,16 @@ from tqdm import *
 
 
 '''
+preliminaries'''
+
+# where are the eads?
+ead_folder = 'C:/Users/eckardm/vandura/Real_Masters_all'
+
+# counter
+counter = 0
+
+
+'''
 go through eads and look for numbers that might be dates'''
 
 # go through each of the files in the ead folder
@@ -25,3 +35,38 @@ for filename in tqdm(os.listdir(ead_folder)):
         # create an etree (part of lxml) tree that we can parse
         ead_tree = etree.parse(join(ead_folder, filename))
         
+        # go through unittitles
+        for unittitle in ead_tree.xpath('//unittitle'):
+            # convert them to a string
+            unittitle_string = etree.tostring(unittitle)
+            # get rid of unitdate tags
+            unittitle_string_without_unitdate = re.sub('(<unitdate.*>).*(<\/unitdate>)', '', unittitle_string)
+            
+            # find numbers that might be dates
+            numbers_that_might_be_dates = re.findall('\s\d{4}\-?\d{4}?', unittitle_string_without_unitdate)
+          
+            # go through them
+            for number_that_might_be_date in numbers_that_might_be_dates:
+            
+                               
+               # if it's not a range
+                if "-" not in number_that_might_be_date:
+                    # see if they are in the appropriate date range and don't have quotes
+                    if 1800 <= int(number_that_might_be_date[:-4]) <= 2015 and not unittitle_string_without_unitdate.split(number_that_might_be_date)[1].startswith('"'):
+                        # add one to the counter
+                        counter += 1
+             
+             # if it is a range
+                else:
+                    # get the start date
+                    number_that_might_be_date_start = number_that_might_be_date.split('-')[0].strip()
+                    # get the end date
+                    number_that_might_be_date_end = number_that_might_be_date.split('-')[1]
+                    # see if they are in the appropriate date range and don't have quotes
+                    if 1800 <= int(number_that_might_be_date_start) and int(number_that_might_be_date_end) <= 2015 and not unittitle_string_without_unitdate.split(number_that_might_be_date_end)[1].startswith('"'):
+                        # add one to the counter
+                        counter += 1
+                        
+# print it out
+print 'found: ' + str(counter)
+  
