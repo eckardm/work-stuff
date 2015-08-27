705428701d9f93c965de3483c9df1305ea390298
Max Eckard <eckardm@umich.edu>
2015-08-27 10:12:34 -0400

    adding re and staring to go through eads

--- numbers_that_might_be_dates.py
+++ numbers_that_might_be_dates.py
@@ -8,8 +8,20 @@ from lxml import etree
 import os
 from os.path import join
 
+# re provide regular expression matching operations
+import re
+
 # tqdm adds a progress meter to loops, you'll need to install it
 from tqdm import *
 
+
 '''
 go through eads and look for numbers that might be dates'''
+
+# go through each of the files in the ead folder
+for filename in tqdm(os.listdir(ead_folder)):
+    # but only do the ones that are actually eads (we can tell because they are xml files)
+    if filename.endswith('.xml'):
+        # create an etree (part of lxml) tree that we can parse
+        ead_tree = etree.parse(join(ead_folder, filename))
+        
