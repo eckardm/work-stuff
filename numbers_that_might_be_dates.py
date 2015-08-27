1fb6713c96462f8ae2365ad9a199f6f42a643f05
Max Eckard <eckardm@umich.edu>
2015-08-27 10:07:36 -0400

    added stuff for importing

--- numbers_that_might_be_dates.py
+++ numbers_that_might_be_dates.py
@@ -1,6 +1,15 @@
 '''
 first, import what we need'''
 
+# lxml is a powerful xml document parser, you'll need to download it
+from lxml import etree
+
+# os provides a portable way of using operating system dependent functionality
+import os
+from os.path import join
+
+# tqdm adds a progress meter to loops, you'll need to install it
+from tqdm import *
 
 '''
 go through eads and look for numbers that might be dates'''
