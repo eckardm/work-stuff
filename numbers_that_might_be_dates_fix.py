655842e50def3ae582df445066afc90305632329
Max Eckard <eckardm@umich.edu>
2015-09-29 16:40:35 -0400

    preliminaries

--- numbers_that_might_be_dates_fix.py
+++ numbers_that_might_be_dates_fix.py
@@ -10,3 +10,16 @@ from os.path import join
 
 # lxml is a powerful and pythonic xml processing library
 from lxml import etree as ET
+
+
+'''
+preliminaries'''
+
+# where is the corrected csv?
+numbers_that_might_be_dates = 'numbers_that_might_be_dates_report-no-typos-no-not-dates.csv'
+
+# where are the test eads?
+test_eads = 'C:/Users/eckardm/Public/Documents/Real_Masters_all'
+
+#where are the production eads?
+production_eads = 'C:/Users/eckardm/GitHub/vandura/Real_Masters_all'
