afc27e167e37c04dd0b3d7f40647aecf97d81335
Max Eckard <eckardm@umich.edu>
2015-09-29 17:02:54 -0400

    correct dates

--- numbers_that_might_be_dates_fix.py
+++ numbers_that_might_be_dates_fix.py
@@ -23,3 +23,49 @@ test_eads = 'C:/Users/eckardm/Public/Documents/Real_Masters_all'
 
 #where are the production eads?
 production_eads = 'C:/Users/eckardm/GitHub/vandura/Real_Masters_all'
+
+
+'''
+use the csv to correct the numbers that might be dates'''
+
+# open the corrected csv in read mode
+with open(numbers_that_might_be_dates, 'r') as corrected_csv:
+    # read it to get the data
+    corrected_data = csv.reader(corrected_csv)
+    
+    # skip the first row
+    next(corrected_data)
+    
+    # go through each row
+    for row in corrected_data:
+    
+        # match up fields to row index
+        filename = row[0]
+        xpath = row[1]
+        number_that_is_date = row[2]
+        context = row[4]
+        
+        # break down number that might be dates
+        begin_date = number_that_is_date[:4]
+        end_date = number_that_is_date[-4:]
+
+        # account for bulk
+        if 'primarily ' in context or 'mainly ' in context or 'bulk ' in context:
+            type = "bulk"
+        else:
+            type = "inclusive"
+        
+        # normalized
+        normal = begin_date + '/' + end_date
+        
+        # corrected date, accounting for certainty
+        if 'ca. ' in context:
+            corrected_date = '<unitdate normal="' + normal + '" type="' + type + '" certainty="approximate">' + number_that_is_date + '</unitdate>'
+        else:
+            corrected_date = '<unitdate normal="' + normal + '" type="' + type + '">' + number_that_is_date + '</unitdate>'
+            
+        print corrected_date
+
+
+'''
+update the finding aids'''
