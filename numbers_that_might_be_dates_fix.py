79dfc77a8f9ded46470d1ccbe908f545fa9dd060
Max Eckard <eckardm@umich.edu>
2015-09-29 16:35:51 -0400

    new csv and script to update dates

---
+++ numbers_that_might_be_dates_fix.py
@@ -0,0 +1,12 @@
+'''
+first, import what we need'''
+
+# csv implements classes to read and write tabular data in csv format
+import csv
+
+# os provides a portable way of using operating system dependent functionality
+import os
+from os.path import join
+
+# lxml is a powerful and pythonic xml processing library
+from lxml import etree as ET
--- saa15.py
+++ saa15.py
@@ -20,24 +20,29 @@ import time
 '''
 go through events on saa page, go to the event, scrape title and description'''
 
-base_url = 'http://archives2015.sched.org/'
-
-education_sessions = urllib2.urlopen(base_url + 'overview/type/education+session#.VfnN3EZuPhW').read()
+education_sessions = urllib2.urlopen('http://archives2015.sched.org/overview/type/education+session#.VfnN3EZuPhW').read()
 education_sessions_soup = BeautifulSoup(education_sessions)
 
 education_session_urls = education_sessions_soup.find_all('a', {'class': 'name'})
 
 for education_session_url in education_session_urls:
-    print education_session_url['href']
+
+
+    education_session = urllib2.urlopen('http://archives2015.sched.org' + education_session_url['href']).read()
+    education_session_soup = BeautifulSoup(education_session)
     
-    # education_session = urllib2.urlopen(base_url + event).read()
-    # education_session_soup = BeautifulSoup(education_session)
+    title = education_session_soup.find('a', {'class': 'name'})
     
-    # title = education_session_soup.find('a', {'class': 'name'})
+    print "TITLE: " + title.get_text()
     
-    # print title
+    description = education_session_soup.find('div', {'class': 'tip-description'})
     
-    # description = education_session_soup.find('div', {'class': 'tip-description'})
-    # print description.text
+    FIX THIS REGEX AND YOU'VE GOT IT!
+    
+    description = re.sub(r'</?\[\d+>', '', description)
+    
+    print "DESCRIPTION: " + description.
+    
+    time.sleep(30) 
     
     
