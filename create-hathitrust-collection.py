435286eed210516878d08b86ba4b033a899da631
Max Eckard <eckardm@umich.edu>
2015-10-02 15:45:21 -0400

    importing for creating ht collections

---
+++ create-hathitrust-collection.py
@@ -0,0 +1,7 @@
+'''
+import what we need'''
+
+# selenium is used automate web browser interaction, you'll need to install it
+from selenium import webdriver
+
+from these-are-not-passwords import *
