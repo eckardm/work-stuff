61d835f415f6360146a4526a00f868384ef0bde7
Max Eckard <eckardm@umich.edu>
2015-10-14 13:17:10 -0400

    cleaning up

--- server-logs/eadids-and-searches.py
+++ server-logs/eadids-and-searches.py
@@ -9,114 +9,130 @@ from os.path import join
 import csv
 
 
-import re
-
 '''
 preliminaries'''
 
 # where are the logs?
 path = r'C:\Users\Public\Documents\server-logs'
 
-# set up some empty histogram dictionaries
-eadid_histogram_total = {}
-searches_histogram_total = {}
-eadid_histogram_total_no_bhl = {}
-searches_histogram_total_no_bhl = {}
+def searches(path):
 
-'''
-first, let's break up the csv by days for total users'''
+	# set up some empty histogram dictionaries
 
-# go through each file in logs
-for filename in os.listdir(path):
-	# if it's a parsed total csv
-	if filename.endswith('_parsed.csv'):
+	searches_histogram_total = {}
 
-		with open('searches.txt', 'w') as text_file:
+	searches_histogram_total_no_bhl = {}
 
-			# open it
-			with open(join(path, filename), 'rb') as csv_file:
-				# read it
-				reader = csv.reader(csv_file)
-				# skip the first line
-				next(reader, None)
 
-				# go through each row
-				for row in reader: 
+	'''
+	get the searches for total users'''
 
-					request = row[2]
+	# go through each file in logs
+	for filename in os.listdir(path):
+		# if it's a parsed total csv
+		if filename.endswith('_parsed.csv'):
 
-					if '&Submit=Search' in request and 'q3=' in request and 'q3=&' not in request:
-						search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0] + ' ' + request.split('op3=')[1].split('&')[0].upper() + ' ' + request.split('q3=')[1].split('&')[0]
-						search = search.replace('AND  AND', 'AND EMPTY AND')
-						if search not in searches_histogram_total:
-							searches_histogram_total[search] = 1
-						else:
-							searches_histogram_total[search] += 1
-						with open('searches.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+			with open('searches.txt', 'w') as text_file:
 
-					elif '&Submit=Search' in request and 'q2=' in request and '&q2=&' not in request:
-						search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0]
-						if search not in searches_histogram_total:
-							searches_histogram_total[search] = 1
-						else:
-							searches_histogram_total[search] += 1
-						with open('searches.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+				# open it
+				with open(join(path, filename), 'rb') as csv_file:
+					# read it
+					reader = csv.reader(csv_file)
+					# skip the first line
+					next(reader, None)
 
-					elif '&Submit=Search' in request:
-						search = request.split('q1=')[1].split('&')[0]
-						if search not in searches_histogram_total:
-							searches_histogram_total[search] = 1
-						else:
-							searches_histogram_total[search] += 1
-						with open('searches.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+					# go through each row
+					for row in reader: 
 
-	# if it's a parsed total csv
-	if filename.endswith('_parsed-noBHL.csv'):
+						# requests
+						request = row[2]
 
-		with open('searches-noBHL.txt', 'w') as text_file:
+						# advanced searches with three searches
+						if '&Submit=Search' in request and 'q3=' in request and 'q3=&' not in request:
+							search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0] + ' ' + request.split('op3=')[1].split('&')[0].upper() + ' ' + request.split('q3=')[1].split('&')[0]
+							search = search.replace('AND  AND', 'AND EMPTY AND')
+							if search not in searches_histogram_total:
+								searches_histogram_total[search] = 1
+							else:
+								searches_histogram_total[search] += 1
+							with open('searches.txt', 'a') as text_file:
+								text_file.write(search + '\n')
 
-			# open it
-			with open(join(path, filename), 'rb') as csv_file:
-				# read it
-				reader = csv.reader(csv_file)
-				# skip the first line
-				next(reader, None)
+						# advanced searches with two	
+						elif '&Submit=Search' in request and 'q2=' in request and '&q2=&' not in request:
+							search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0]
+							if search not in searches_histogram_total:
+								searches_histogram_total[search] = 1
+							else:
+								searches_histogram_total[search] += 1
+							with open('searches.txt', 'a') as text_file:
+								text_file.write(search + '\n')
 
-				# go through each row
-				for row in reader: 
+						# advanced searches or just plain searches with just one
+						elif '&Submit=Search' in request:
+							search = request.split('q1=')[1].split('&')[0]
+							if search not in searches_histogram_total:
+								searches_histogram_total[search] = 1
+							else:
+								searches_histogram_total[search] += 1
+							with open('searches.txt', 'a') as text_file:
+								text_file.write(search + '\n')
 
-					request = row[2]
 
-					if '&Submit=Search' in request and 'q3=' in request and 'q3=&' not in request:
-						search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0] + ' ' + request.split('op3=')[1].split('&')[0].upper() + ' ' + request.split('q3=')[1].split('&')[0]
-						search = search.replace('AND  AND', 'AND EMPTY AND')
-						if search not in searches_histogram_total_no_bhl:
-							searches_histogram_total_no_bhl[search] = 1
-						else:
-							searches_histogram_total_no_bhl[search] += 1
-						with open('searches-noBHL.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+		'''
+		get the searches for total users no bhl'''
 
-					elif '&Submit=Search' in request and 'q2=' in request and '&q2=&' not in request:
-						search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0]
-						if search not in searches_histogram_total_no_bhl:
-							searches_histogram_total_no_bhl[search] = 1
-						else:
-							searches_histogram_total_no_bhl[search] += 1
-						with open('searches-noBHL.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+		# if it's a parsed total csv
+		if filename.endswith('_parsed-noBHL.csv'):
 
-					elif '&Submit=Search' in request:
-						search = request.split('q1=')[1].split('&')[0]
-						if search not in searches_histogram_total_no_bhl:
-							searches_histogram_total_no_bhl[search] = 1
-						else:
-							searches_histogram_total_no_bhl[search] += 1
-						with open('searches-noBHL.txt', 'a') as text_file:
-							text_file.write(search + '\n')
+			with open('searches-noBHL.txt', 'w') as text_file:
 
-print searches_histogram_total
-print searches_histogram_total_no_bhl
+				# open it
+				with open(join(path, filename), 'rb') as csv_file:
+					# read it
+					reader = csv.reader(csv_file)
+					# skip the first line
+					next(reader, None)
+
+					# go through each row
+					for row in reader: 
+
+						# requests
+						request = row[2]
+
+						# advanced searches with three searches
+						if '&Submit=Search' in request and 'q3=' in request and 'q3=&' not in request:
+							search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0] + ' ' + request.split('op3=')[1].split('&')[0].upper() + ' ' + request.split('q3=')[1].split('&')[0]
+							search = search.replace('AND  AND', 'AND EMPTY AND')
+							if search not in searches_histogram_total_no_bhl:
+								searches_histogram_total_no_bhl[search] = 1
+							else:
+								searches_histogram_total_no_bhl[search] += 1
+							with open('searches-noBHL.txt', 'a') as text_file:
+								text_file.write(search + '\n')
+
+						# advanced searches with two
+						elif '&Submit=Search' in request and 'q2=' in request and '&q2=&' not in request:
+							search = request.split('q1=')[1].split('&')[0] + ' ' + request.split('op2=')[1].split('&')[0].upper() + ' ' + request.split('q2=')[1].split('&')[0]
+							if search not in searches_histogram_total_no_bhl:
+								searches_histogram_total_no_bhl[search] = 1
+							else:
+								searches_histogram_total_no_bhl[search] += 1
+							with open('searches-noBHL.txt', 'a') as text_file:
+								text_file.write(search + '\n')
+
+						# advanced searches or just plain searches with just one
+						elif '&Submit=Search' in request:
+							search = request.split('q1=')[1].split('&')[0]
+							if search not in searches_histogram_total_no_bhl:
+								searches_histogram_total_no_bhl[search] = 1
+							else:
+								searches_histogram_total_no_bhl[search] += 1
+							with open('searches-noBHL.txt', 'a') as text_file:
+								text_file.write(search + '\n')
+
+	'''
+	for now, just print the searches, in the future these should be historgrams?'''
+
+	print searches_histogram_total
+	print searches_histogram_total_no_bhl
