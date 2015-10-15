8c4296bd8b488ce1bc4f716e7c4a3c8d237a0cf2
Max Eckard <eckardm@umich.edu>
2015-10-15 10:17:36 -0400

    now matches eadid to title with dicitonary

---
+++ server-logs/eadids-and-unittitles.py
@@ -0,0 +1,59 @@
+# import what we need
+import os
+from os.path import join
+from lxml import etree
+import re
+from tqdm import *
+
+#empty dictionary for ead identifier keys and title proper values
+eadids_and_unittitles = {}
+
+# where are the eads?
+ead_path = 'C:/Users/eckardm/vandura/Real_Masters_all'
+
+# xpaths we'll need
+eadid_xpath = '//eadid'
+collection_unittitle_xpath = '//did/unittitle'
+
+# print that we're creating the dictionary
+print 'Creating dictionary.'
+
+# start a counter
+counter = 0
+
+# go through the eads
+for filename in tqdm(os.listdir(ead_path)):
+    if filename.endswith('xml'):
+
+        # parse the ead
+        ead_tree = etree.parse(join(ead_path, filename))
+        # get the eadid xml entity
+        eadid = ead_tree.xpath(eadid_xpath)
+        # get the collection unit title xml entity
+        collection_unittitle = ead_tree.xpath(collection_unittitle_xpath)
+        # get the eadid text
+        eadid_text = eadid[0].text
+        # get the collection unit title text, and get the line breaks and crazy spacing right
+        collection_unittitle_text = etree.tostring(collection_unittitle[0])
+        # more crazy spacing
+        collection_unittitle_text = ' '.join(collection_unittitle_text.split())
+        # remove the xml tags <-- this probably isn't the most elegant way to do this
+        collection_unittitle_text = re.sub('<.*?>', '', collection_unittitle_text)
+        # add both do the ead identifier and title proper dictionary
+        eadids_and_unittitles[eadid_text] = collection_unittitle_text
+        # update counter
+        counter += 1
+
+# where is constants.py?
+constants = 'constants.py'
+# get the time (http://stackoverflow.com/questions/13890935/timestamp-python)
+import time
+rightnow = time.time()
+import datetime
+datetime_rightnow = datetime.datetime.fromtimestamp(rightnow).strftime('%Y-%m-%d %H:%M:%S')
+# put the dictionary in constants.py
+with open(constants, "a") as txt_file:
+    txt_file.write('# dictionary for eadid (key) and titleproper (value) accruate as of ' + datetime_rightnow + '\neadids_and_unittitles = ' + str(eadids_and_unittitles))
+
+# print that dictionary got created
+print '\rDictionary with ' + str(counter) + ' entries and accurate as of ' + datetime_rightnow + ' created.'
