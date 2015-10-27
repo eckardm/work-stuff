4a082b92c3ef5edda51e94ac1945d480e34bf2cf
Max Eckard <eckardm@umich.edu>
2015-10-27 11:16:51 -0400

    cleaning up

--- deep-blue-xml.py
+++ deep-blue-xml.py
@@ -2,30 +2,14 @@ import lxml.etree as ET
 from lxml.builder import E
 import csv
 
-from collections import defaultdict
+from coll_item_nos_and_dig_file_calcs import coll_item_nos_and_dig_file_calcs
 
 
 programs = ['8730', '90139', '87334', '87250', '8738']
 
 errors = []
 
-coll_item_nos = []
-with open(r'C:\Users\Public\Documents\audioDigitalPreservationItemView\audioDigitalPreservationItemView.mer') as csv_file:
-	reader = csv.DictReader(csv_file)
-	for row_dict in reader:
-		if row_dict["CollItemNo"] and row_dict["CollItemNo"] not in coll_item_nos:
-			coll_item_nos.append(row_dict["CollItemNo"])
 
-coll_item_nos_and_dig_file_calcs = defaultdict(list)
-for i in coll_item_nos:
-	with open(r'C:\Users\Public\Documents\audioDigitalPreservationItemView\audioDigitalPreservationItemView.mer') as csv_file:
-		reader = csv.DictReader(csv_file)
-		for row_dict in reader:
-			if row_dict["CollItemNo"] == i:
-				coll_item_nos_and_dig_file_calcs[i].append(row_dict["DigFile Calc"])
-
-with open('coll_item_nos_and_dig_file_calcs.py', 'w') as txt_file:
-	txt_file.write(str(coll_item_nos_and_dig_file_calcs))
 	
 '''
 
