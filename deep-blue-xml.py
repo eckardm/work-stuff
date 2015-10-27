ed2767710b3b473391b007ae7a6cf3c513ce47fb
Max Eckard <eckardm@umich.edu>
2015-10-27 11:25:12 -0400

    abstracts work!

--- deep-blue-xml.py
+++ deep-blue-xml.py
@@ -4,17 +4,11 @@ import csv
 
 from coll_item_nos_and_dig_file_calcs import coll_item_nos_and_dig_file_calcs
 
-
 programs = ['8730', '90139', '87334', '87250', '8738']
 
 errors = []
 
-
-	
-'''
-
 try:
-
 	with open(r'C:\Users\Public\Documents\audioDigitalPreservationItemView\audioDigitalPreservationItemView.mer') as csv_file:
 		reader = csv.DictReader(csv_file)
 		for row_dict in reader:
@@ -33,40 +27,28 @@ try:
 					E("dc.date.open", "2015"),
 					E("dc.description", "Content note: The sound recording(s) associated with this repository item derive from a single audio reel tape. A single tape may yield multiple audio files if there were variations in tape stock, speed, or channels (i.e., stereo or mono). For more information see: http://deepblue.lib.umich.edu/handle/2027.42/108126."),
 					)
-				digital_file_components = row_dict["DigFile Calc"].split('-')
-				if row_dict["DigFile Calc"].split('-')[0] in programs:
-					if len(digital_file_components) == 6:
-						bracket_info = "[Program " + row_dict["DigFile Calc"].split('-')[3] + "] : [Part " + row_dict["DigFile Calc"].split('-')[4] + "] : [Segment " + row_dict["DigFile Calc"].split('-')[5] + "] : "
-					elif len(digital_file_components) == 5:
-						bracket_info = "[Program " + row_dict["DigFile Calc"].split('-')[3] + "] : [Part " + row_dict["DigFile Calc"].split('-')[4] + "] : "
-				else:
-					if len(digital_file_components) == 5:
-						bracket_info = "[Part " + row_dict["DigFile Calc"].split('-')[3] + "] : [Segment " + row_dict["DigFile Calc"].split('-')[4] + "] : "
-					elif len(digital_file_components) == 4:
-						bracket_info = "[Part " + row_dict["DigFile Calc"].split('-')[3] + "] : "
-				abstract = bracket_info + row_dict["NoteContent"]
-				audio_item.append(
-					E("dc.abstract", abstract)
-					)
-	#		print bracket_info
-		#	if row_dict["DigFile Calc"].split('-')[0] in programs:
-		#		bracket_info = "[Program " + row_dict["DigFile Calc"].split('-')[3] + "] : [Part " + row_dict["DigFile Calc"].split('-')[4] + "] : [Segment " + row_dict["DigFile Calc"].split('-')[5] + "]"
-		#	else: 
-		#		bracket_info = part = "[Part " + row_dict["DigFile Calc"].split('-')[4] + "] : [Segment " + row_dict["DigFile Calc"].split('-')[5] + "]"
-		#	print bracket_info
 
+				for dig_file_calc in coll_item_nos_and_dig_file_calcs[row_dict["CollItemNo"]]:
+					digital_file_components = dig_file_calc.split('-')
+					if dig_file_calc.split('-')[0] in programs:
+						if len(digital_file_components) == 6:
+							bracket_info = "[Program " + dig_file_calc.split('-')[3] + "] : [Part " + dig_file_calc.split('-')[4] + "] : [Segment " + dig_file_calc.split('-')[5] + "] : "
+						elif len(digital_file_components) == 5:
+							bracket_info = "[Program " + dig_file_calc.split('-')[3] + "] : [Part " + dig_file_calc.split('-')[4] + "] : "
+					else:
+						if len(digital_file_components) == 5:
+							bracket_info = "[Part " + dig_file_calc.split('-')[3] + "] : [Segment " + dig_file_calc.split('-')[4] + "] : "
+						elif len(digital_file_components) == 4:
+							bracket_info = "[Part " + dig_file_calc.split('-')[3] + "] : "
+					abstract = bracket_info + row_dict["NoteContent"]
+					audio_item.append(E("dc.abstract", abstract))
 
-			#	E("dc.abstract", row_dict["NoteContent"]), # need info in []s, repeatable
-				#E("bitstream", # based on CollItemNo--need dictionary of which ones have programs
-				#	E("dc.title.filename"),
-				#	E("dc.description.filename"),
-				#	E("dc.format.mimetype"),
-				#	)
-				#)
+			
 			ET.dump(audio_item)
-#	errors.append(row_dict["DigFile Calc"])
+
 except:
-	errors.append(row_dict["DigFile Calc"])
+ 	errors.append(row_dict["DigFile Calc"])
+
 print 'errors'
 for i in errors:
-		print i'''
+		print i
