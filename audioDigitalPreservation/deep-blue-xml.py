8e9c76c18a6a3a7a32015e34b6664973f004bcf9
Max Eckard <eckardm@umich.edu>
2015-10-28 11:13:11 -0400

    commenting it up

--- audioDigitalPreservation/deep-blue-xml.py
+++ audioDigitalPreservation/deep-blue-xml.py
@@ -1,25 +1,34 @@
-
+# lxml is a pythonic binding for the c libraries libxml2 and libxslt
 import lxml.etree as ET
 from lxml.builder import E
+
+# csv implements classes to read and write tabular data in csv format
 import csv
+
+# os provides a portable way of using operating system dependent functionality
 import os
 
+# we made these earlier
 from coll_item_nos_and_dig_file_calcs import coll_item_nos_and_dig_file_calcs
 from coll_item_nos_and_audio_genres import coll_item_nos_and_audio_genres
-from batches import batches
 from coll_item_nos_and_jpegs import coll_item_nos_and_jpegs
+from batches import batches
 
-
+# a list of programs, because these are exceptions to the rule
 programs = ['8730', '90139', '87334', '87250', '8738']
 
+# there is one encoding error that keeps throwing this off, for the time being, i'm putting it here
 errors = []
 
+# try statement is because of the encoding error, i intend to get rid of it
 try:
+	# open the export from beal and go through it
 	with open(r'C:\Users\Public\Documents\audioDigitalPreservationItemView\audioDigitalPreservationItemView.mer') as csv_file:
 		reader = csv.DictReader(csv_file)
 		for row_dict in reader:
 			if row_dict["CollItemNo"]:
-				
+
+				# start constructing the xml
 				audio_item = E.audio_item(
 					E("dc.identifier.other", row_dict["CollItemNo"]),
 					E("dc.title", "Sound Recording - " + row_dict["ItemTitle"]),
@@ -27,6 +36,7 @@ try:
 					E("dc.date.issued", "2015"),
 					E("dc.date.created", row_dict["Files_Return_Date"][-4:]),
 					E("dc.coverage.temporal", row_dict["ItemDate"]),
+					# this seems inelegant
 					E("dc.type", str(coll_item_nos_and_audio_genres[row_dict["CollItemNo"]]).replace('[', '').replace(']', '').replace("'", '').replace(',', ';')),
 					# E("dc.description.restriction"),
 					E("dc.rights.access", "This material is available for research only in the Reading Room of the Bentley Historical Library at the University of Michigan (Ann Arbor, MI)."),
@@ -35,9 +45,10 @@ try:
 					E("dc.description", "Content note: The sound recording(s) associated with this repository item derive from a single audio reel tape. A single tape may yield multiple audio files if there were variations in tape stock, speed, or channels (i.e., stereo or mono). For more information see: http://deepblue.lib.umich.edu/handle/2027.42/108126.")
 					)
 
+				# this goes through each digital file associated with a collection item
 				for dig_file_calc in coll_item_nos_and_dig_file_calcs[row_dict["CollItemNo"]]:
 
-					# parsing digital file calculations to get descriptions
+					# parsing digital file calculations to get descriptions, this is fairly inelegant
 					if dig_file_calc.split('-')[0] in programs:
 						if len(dig_file_calc.split('-')) == 6:
 							description = "[Program " + dig_file_calc.split('-')[3] + "] : [Part " + dig_file_calc.split('-')[4] + "] : [Segment " + dig_file_calc.split('-')[5] + "]"
@@ -100,6 +111,7 @@ try:
 				)
 
 				# jpegs
+				# goes through each jpeg associated with folders on the nas box
 				for jpeg in (coll_item_nos_and_jpegs[row_dict["DigFile Calc"]]):
 					audio_item.append(
 						E.bitstream(
@@ -109,8 +121,10 @@ try:
 						)
 					)
 			
+			# for now, dumping these
 			ET.dump(audio_item)
 
+# and, keeping up with errors
 except:
  	errors.append(row_dict["DigFile Calc"])
 
