18ade1ba14b79cdb7c6b848b901305e2516a6c51
Max Eckard <eckardm@umich.edu>
2015-11-13 14:34:45 -0500

    playing with youtube-dl

--- youtube-dl.py
+++ youtube-dl.py
@@ -1,1 +1,6 @@
-from future
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {'writeinfojson': True}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/user/practicalkatabunkai/'])
