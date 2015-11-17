5159bf5aad2f34856d18e2cfc1f973cefd9b504d
Max Eckard <eckardm@umich.edu>
2015-11-17 12:16:14 -0500

    quick look at clustering

---
+++ agents/cluster.py
@@ -0,0 +1,21 @@
+import os
+from os.path import join
+from lxml import etree
+import csv
+
+path = r'C:\Users\eckardm\vandura\Real_Masters_all'
+
+origination_xpath =  '//origination/*'
+controlaccess_xpath = '//controlaccess/*'
+
+
+for filename in os.listdir(path):
+	if filename.endswith('xml'):
+		print filename
+		ead_tree = etree.parse(join(path, filename))
+		elements = ead_tree.xpath(origination_xpath) + ead_tree.xpath(controlaccess_xpath)
+		for element in elements:
+			if element.tag in ["famname", "corpname", "persname"]:
+				with open('cluster.csv', 'ab') as csv_file:
+					writer = csv.writer(csv_file)
+					writer.writerow([element.text.encode('utf-8')])
