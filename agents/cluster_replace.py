299311b05e2576d65a1095a5d75844aeda74ee42
Max Eckard <eckardm@umich.edu>
2015-11-17 13:55:12 -0500

    updates

--- agents/cluster_replace.py
+++ agents/cluster_replace.py
@@ -42,7 +42,7 @@ for filename in os.listdir(path):
             agent.text = dictionary[agent_text].encode('utf-8')
 
 
-        with open(join(path, filename), 'w') as see_i_am_making_all_things_new:
-          see_i_am_making_all_things_new.write(etree.tostring(tree, encoding='utf-8', xml_declaration=True))
+            with open(join(path, filename), 'w') as see_i_am_making_all_things_new:
+              see_i_am_making_all_things_new.write(etree.tostring(tree, encoding='utf-8', xml_declaration=True))
 
 print "That's it, we're done!"
