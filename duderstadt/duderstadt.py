5c14a412b55c00266706a5513c9b50dadc275a55
Max Eckard <eckardm@umich.edu>
2015-12-02 09:55:47 -0500

    spacing

--- duderstadt/duderstadt.py
+++ duderstadt/duderstadt.py
@@ -21,21 +21,26 @@ for anchor in tqdm(soup("a")):
         os.mkdir("duderstadt" + "/" + anchor["href"])
         subgroup_data = requests.get(base_url + anchor["href"], headers=headers)
         subgroup_soup = BeautifulSoup(subgroup_data.text)
+        
         for subgroup_anchor in subgroup_soup("a"):
             if subgroup_anchor.text.strip() != "Parent Directory":
                 os.mkdir("duderstadt" + "/" + anchor["href"] + "/" + subgroup_anchor["href"])
                 digital_documents_data = requests.get(base_url + anchor["href"] + subgroup_anchor["href"], headers=headers)
                 digital_documents_soup = BeautifulSoup(digital_documents_data.text)
+                
                 for digital_document_anchor in digital_documents_soup("a"):
                     if "restricted" in digital_document_anchor.text:
                         os.mkdir("duderstadt" + "/" + anchor["href"] + "/" + subgroup_anchor["href"] + "/" + digital_document_anchor["href"])
                         restricted_digital_documents_data = requests.get(base_url + anchor["href"] + subgroup_anchor["href"] + digital_document_anchor["href"])
                         restricted_digital_documents_soup = BeautifulSoup(restricted_digital_documents_data.text)
+                        
                         for restricted_digital_documents_anchor in restricted_digital_documents_soup("a"):
                             if restricted_digital_documents_anchor.text.strip() != "Parent Directory":
                                 urllib.urlretrieve(base_url + anchor["href"] + subgroup_anchor["href"] + digital_document_anchor["href"] + restricted_digital_documents_anchor["href"], "duderstadt" + "/" + anchor["href"] + "/" + subgroup_anchor["href"] + "/" + digital_document_anchor["href"] + "/" + restricted_digital_documents_anchor["href"])
+                                
                     elif digital_document_anchor.text.strip() != "Parent Directory":
                         urllib.urlretrieve(base_url + anchor["href"] + subgroup_anchor["href"] + digital_document_anchor["href"], "duderstadt" + "/" + anchor["href"] + "/" + subgroup_anchor["href"] + "/" + digital_document_anchor["href"])
+                        
                     time.sleep(1)'''
 
 # write headers for metadata
