a5ec9604cb689004cdb2b36b9fa22b2d4bffccca
Max Eckard <eckardm@umich.edu>
2015-12-02 09:55:06 -0500

    commenting and starting new

--- duderstadt/duderstadt.py
+++ duderstadt/duderstadt.py
@@ -7,6 +7,7 @@ import time
 import csv
 
 '''
+# scrape data
 base_url = "http://bentley.umich.edu/elecrec/d/duderstadt/"
 
 headers = {"user-agent": "Duderstadt Scraper: eckardm at umich (dot) com"}
@@ -37,6 +38,7 @@ for anchor in tqdm(soup("a")):
                         urllib.urlretrieve(base_url + anchor["href"] + subgroup_anchor["href"] + digital_document_anchor["href"], "duderstadt" + "/" + anchor["href"] + "/" + subgroup_anchor["href"] + "/" + digital_document_anchor["href"])
                     time.sleep(1)'''
 
+# write headers for metadata
 metadata_headers = [
     "IDENTIFIER.OTHER", 
     "DC.TITLE", 
@@ -60,44 +62,8 @@ with open("deepBlue_9811_0003.csv", mode="wb") as metadata:
     for metadata_header in metadata_headers:
         writer.writerow(metadata_header)
 
+# identify restricted files
 
+# identify converted files
 
-converted_files = {}
-with open("convertedFiles_9811_0003.csv", mode="r") as converted_files_csv:
-    reader = csv.reader(converted_files_csv)
-    for row in reader:
-        input = os.path.split(row[0].split("; ")[1].strip())[1].replace('"', "")
-        output = os.path.split(row[0].split("; ")[2].strip())[1].replace('"', "")
-        if input in converted_files:
-            print input, output, converted_files[input]
-        converted_files[input] = output
-          
-
-print len(converted_files)
-test = []
-for root, dirs, files in os.walk("C:\Users\eckardm\work-stuff\duderstadt\duderstadt"):
-    for name in files:
-        if name in converted_files:
-            test.append(name)
-print len(test)
-
-for name in converted_files:
-    if name not in test:
-        print "what?", name
-
-
-restricted_files = []
-'''
-        if filename.split(".")[0] + "-bhl_" in os.listdir("path/to/directory"):
-            row.append("9811_0003_0001")
-            row.append(title)
-            row.append("Duderstadt, James J.")
-            row.append("")
-            row.append("2015-11-25")
-            row.append(date)
-        if "-bhl_" in filename:
-            row.append(filename.split("-bhl_")[0] + filename[-4:] + "|" + filename)
-        writer.writerow( title, "", "Duderstadt, James J.", "", "2015-11-25", date, , "Access version | Preservation version", ""])
-        
-        with open("deepBlue_9811_0003", mode="ab") as metadata:
-    writer = csv.writer(metadata)'''
+# write metadata
