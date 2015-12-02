4a0decc43637049d6f825e167990a1e1334c905f
Max Eckard <eckardm@umich.edu>
2015-12-02 17:07:46 -0500

    typo

--- server-logs/eadids-and-unittitles.py
+++ server-logs/eadids-and-unittitles.py
@@ -53,7 +53,7 @@ import datetime
 datetime_rightnow = datetime.datetime.fromtimestamp(rightnow).strftime('%Y-%m-%d %H:%M:%S')
 # put the dictionary in constants.py
 with open(constants, "a") as txt_file:
-    txt_file.write('# dictionary for eadid (key) and titleproper (value) accruate as of ' + datetime_rightnow + '\neadids_and_unittitles = ' + str(eadids_and_unittitles))
+    txt_file.write('# dictionary for eadid (key) and titleproper (value) accurate as of ' + datetime_rightnow + '\neadids_and_unittitles = ' + str(eadids_and_unittitles))
 
 # print that dictionary got created
 print '\rDictionary with ' + str(counter) + ' entries and accurate as of ' + datetime_rightnow + ' created.'
