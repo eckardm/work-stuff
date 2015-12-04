e1401e753fd8ab044955e8c3ace704324c465312
Max Eckard <eckardm@umich.edu>
2015-12-04 11:17:22 -0500

    script debug (thanks walker) and output

--- duderstadt/duderstadt.py
+++ duderstadt/duderstadt.py
@@ -39,84 +39,99 @@ converted_files = {}
 with open("convertedFiles_9811_0003.csv", mode="rb") as converted_files_csv:
     reader = csv.reader(converted_files_csv)
     for row in reader:
-        original_path = row[0].split("; ")[1].replace('"', "").strip()
-        converted_path = row[0].split("; ")[2].replace('"', "").strip()
+        split_data = row[0].split("; ")
+        
+        original_path = split_data[1].strip('" ')
+        converted_path = split_data[2].strip('" ')
+        
         converted_files[original_path] = converted_path
 
 # get metadata
 from metadata import metadata
 
 # write metadata
-'''
-File Name | Author | Date of Publication or Speech | Title
-----------|--------|-------------------------------|------'''
-
 identifier_counter = 1
 
-for root, dirs, files in os.walk("C:\Users\eckardm\work-stuff\duderstadt\9811_0003"):
+for root, _, files in os.walk("C:\Users\eckardm\work-stuff\duderstadt\9811_0003"):
     for name in files:
-        if "_bhl-" not in name:
+        if "_bhl-" in name:
+            continue
         
-            if identifier_counter <= 99:
-                identifier_other = "9811_0003_000" + str(identifier_counter)
-            elif identifier_counter <= 999:
-                identifier_other = "9811_0003_00" + str(identifier_counter)
-            else:
-                identifier_other = "9811_0003_0" + str(identifier_counter)
-            identifier_counter += 1
-            
-            href = "../" + root.split("\\")[-2:][0] + "/" + root.split("\\")[-2:][1] + "/" + name
-            for iter_var in metadata:
-                if href == iter_var["href"]:
-                    title = iter_var["title"]
-                    date = iter_var["date"]
-                    
-            dc_title = title
-            
-            dc_description_abstract = ""
-            
-            dc_contributor_author = "Duderstadt, James J."
-            
-            dc_contributor_other = ""
-            
-            dc_date_issued = "2015"
-            
-            # does this need to be in YYYY-MM-DD format? i'm going to guess probably. what should get passed to finding aids?
-            if "-" in date or date == "undated" or date == "1989":
-                dc_date_created = date
-            else:
-                try:
-                    dc_date_created = datetime.datetime.strptime(date, "%m/%d/%y")
-                # maybe this is the best thing to do? can't tell how these are typos.
-                except:
-                    dc_date_created = datetime.datetime.strptime(date, "%d/%m/%y")
-                dc_date_created = dc_date_created.strftime("%Y-%m-%d")
-            
-            dc_coverage_temporal = ""
-
-            for iter_var in converted_files:
-                if "\\".join(iter_var.split("\\")[4:]) == "\\".join(root.split("\\")[6:]) + "\\" + name:
-                    dc_title_filename = name + " | " + str(converted_files[iter_var].split("\\")[-1:]).replace("'", "").replace("[", "").replace("]", "")
-                    dc_description_filename = "Access version | Preservation version"
-                else:
-                    dc_title_filename = name
-                    dc_description_filename = ""
-            
-            dc_type = "Office Documents"
-            
-            if name in restricted_files:
-                dc_rights_access = "Closed for research use."
-                dc_date_open = "2030-07-01"
-            else:
-                dc_rights_access = "Collection is open for research."
-                # today's date?
-                dc_date_open = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
-            
-            dc_rights_copyright = "Copyright has been transferred to the Regents of the University of Michigan."
-            
-            # i think this is right.
-            dc_language_iso = "en_US"
-            
-            with open("deepBlue_9811_0003.csv", mode="ab") as metadata_csv:
-                writer = csv.writer(metadata_csv)
-                writer.writerow([identifier_other, dc_title, dc_description_abstract, dc_contributor_author, dc_contributor_other, dc_date_issued, dc_date_created, dc_coverage_temporal, dc_title_filename, dc_description_filename, dc_type, dc_rights_access, dc_date_open, dc_rights_copyright, dc_language_iso])
+        base_string = "9811_0003_"
+        number = str(identifier_counter).zfill(4)
+        identifier_other = base_string + number
+        identifier_counter += 1
+        
+        href = "../" + root.split("\\")[-2:][0] + "/" + root.split("\\")[-2:][1] + "/" + name
+        for dct in metadata:
+            if dct["href"] == href:
+                title = dct["title"]
+                date = dct["date"]
+                
+        dc_title = title
+        
+        dc_description_abstract = ""
+        
+        dc_contributor_author = "Duderstadt, James J."
+        
+        dc_contributor_other = ""
+        
+        dc_date_issued = "2015"
+        
+        # does this need to be in YYYY-MM-DD format? i'm going to guess probably. what should get passed to finding aids?
+        if "-" in date or date == "undated" or date == "1989":
+            dc_date_created = date
+        else:
+            try:
+                dc_date_created = datetime.datetime.strptime(date, "%m/%d/%y")
+            # maybe this is the best thing to do? can't tell how these are typos.
+            except:
+                dc_date_created = datetime.datetime.strptime(date, "%d/%m/%y")
+            dc_date_created = dc_date_created.strftime("%Y-%m-%d")
+        
+        dc_coverage_temporal = ""
+        
+        dc_title_filename = name
+        
+        dc_description_filename = ""
+        
+        for original_path in converted_files:
+            converted_file_sub_path = "\\".join(original_path.split("\\")[4:])
+            current_sub_path = "\\".join(root.split("\\")[6:]) + "\\" + name
+            if converted_file_sub_path == current_sub_path:
+                dc_title_filename = name + " | " + converted_files[original_path].split("\\")[-1]
+                dc_description_filename = "Access version | Preservation version"
+        
+        dc_type = "Office Documents"
+        
+        if name in restricted_files:
+            dc_rights_access = "Closed for research use."
+            dc_date_open = "2030-07-01"
+        else:
+            dc_rights_access = "Collection is open for research."
+            # today's date?
+            dc_date_open = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
+        
+        dc_rights_copyright = "Copyright has been transferred to the Regents of the University of Michigan."
+        
+        # i think this is right.
+        dc_language_iso = "en_US"
+        
+        with open("deepBlue_9811_0003.csv", mode="ab") as metadata_csv:
+            writer = csv.writer(metadata_csv)
+            writer.writerow([
+                identifier_other, 
+                dc_title, 
+                dc_description_abstract, 
+                dc_contributor_author, 
+                dc_contributor_other, 
+                dc_date_issued, 
+                dc_date_created, 
+                dc_coverage_temporal, 
+                dc_title_filename, 
+                dc_description_filename, 
+                dc_type, 
+                dc_rights_access, 
+                dc_date_open, 
+                dc_rights_copyright, 
+                dc_language_iso])
