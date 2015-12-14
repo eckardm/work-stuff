2fcaec2c46d7fcbef0b60db61e552a4408765cc5
Max Eckard <eckardm@umich.edu>
2015-12-14 16:59:26 -0500

    done a lot today

--- duderstadt/make_deepblue_metadata.py
+++ duderstadt/make_deepblue_metadata.py
@@ -1,12 +1,11 @@
-import os
 import csv
-import re
-import time
+import cPickle as pickle
+import os
+import shutil
 import datetime
-from fuzzywuzzy import fuzz, process
 
 # write headers for metadata
-with open("deepBlue_9811_0003-TEMP.csv", mode="wb") as metadata_csv:
+with open("deepBlue_9811_0001.csv", mode="wb") as metadata_csv:
     writer = csv.writer(metadata_csv)
     writer.writerow([
     "IDENTIFIER.OTHER", 
@@ -18,6 +17,7 @@ with open("deepBlue_9811_0003-TEMP.csv", mode="wb") as metadata_csv:
     "DC.DATE.CREATED", 
     "DC.COVERAGE.TEMPORAL", 
     "DC.TITLE.FILENAME ", 
+    "LOCATION",
     "DC.DESCRIPTION.FILENAME", 
     "DC.TYPE", 
     "DC.RIGHTS.ACCESS", 
@@ -26,130 +26,105 @@ with open("deepBlue_9811_0003-TEMP.csv", mode="wb") as metadata_csv:
     "DC.LANGUAGE.ISO"
 ])
 
-with open("deepBlue_9811_0003.csv", mode="wb") as metadata_csv:
-    writer = csv.writer(metadata_csv)
-    writer.writerow([
-    "IDENTIFIER.OTHER", 
-    "DC.TITLE", 
-    "DC.DESCRIPTION.ABSTRACT", 
-    "DC.CONTRIBUTOR.AUTHOR", 
-    "DC.CONTRIBUTOR.OTHER", 
-    "DC.DATE.ISSUED", 
-    "DC.DATE.CREATED", 
-    "DC.COVERAGE.TEMPORAL", 
-    "DC.TITLE.FILENAME ", 
-    "DC.DESCRIPTION.FILENAME", 
-    "DC.TYPE", 
-    "DC.RIGHTS.ACCESS", 
-    "DC.DATE.OPEN", 
-    "DC.RIGHTS.COPYRIGHT", 
-    "DC.LANGUAGE.ISO"
-])
+information_packages = pickle.load(open("information_packages.p", mode="rb"))
 
-# identify converted files
-converted_files = {}
+os.mkdir("1995-1996_Speeches-Other")
 
-with open("convertedFiles_9811_0003.csv", mode="rb") as converted_files_csv:
-    reader = csv.reader(converted_files_csv)
-    for row in reader:
-        split_data = row[0].split("; ")
-        
-        original_path = split_data[1].strip('" ')
-        converted_path = split_data[2].strip('" ')
-        
-        converted_files[original_path] = converted_path
+counter = 1
 
-# get metadata
-from metadata import metadata
+for information_package in information_packages:
 
-# write metadata
-identifier_counter = 1
-
-for _, _, files in os.walk("C:\Users\eckardm\work-stuff\duderstadt\9811_0003"):
-    for name in files:
-        if "_bhl-" in name:
-            continue
-        
-        base_string = "9811_0003_"
-        number = str(identifier_counter).zfill(4)
-        identifier_other = base_string + number
-        identifier_counter += 1
-        
+    base_string = "9811_0001_"
+    number = str(counter).zfill(4)
+    identifier_other = base_string + number
+    counter += 1
+    
+    if information_package.get("unittitle", ""):
+        dc_title = information_package.get("series", "") + " - " + information_package.get("subseries", "") + " - " + information_package.get("unittitle", "")
+    else:
         dc_title = ""
+        shutil.copy2(information_package.get("preservation_location"), "1995-1996_Speeches-Other")
+    
+    dc_description_abstract = ""
+    
+    dc_contributor_author = "Duderstadt, James J."
         
-        dc_date_created = ""
+    dc_contributor_other = ""
         
-        href = "../" + "/".join(name.split("-"))
-        for dct in metadata:
-            if dct["href"] == href:
-                dc_title = dct["title"]
-                dc_date_created = dct["date"]
-        
-                # does this need to be in YYYY-MM-DD format? i'm going to guess probably. what should get passed to finding aids?
-                if not dc_date_created:
-                    continue
-                if "-" in dc_date_created or dc_date_created == "undated" or dc_date_created == "1989":
-                    continue
-                try:
-                    dc_date_created = datetime.datetime.strptime(dc_date_created, "%m/%d/%y")
-                # maybe this is the best thing to do? can't tell how these are typos.
-                except:
-                    dc_date_created = datetime.datetime.strptime(dc_date_created, "%d/%m/%y")
-                dc_date_created = dc_date_created.strftime("%Y-%m-%d")
-                
-                
-        dc_description_abstract = ""
-        
-        dc_contributor_author = "Duderstadt, James J."
-        
-        dc_contributor_other = ""
-        
-        dc_date_issued = "2015"
-        
-        dc_coverage_temporal = ""
-        
-        dc_title_filename = name
-        
-        dc_description_filename = ""
-        
-        for original_path in converted_files:
-            converted_file_sub_path = "\\".join(original_path.split("\\")[4:])
-            current_sub_path = "\\".join(name.split("-"))
-            if converted_file_sub_path == current_sub_path:
-                dc_title_filename = name + " | " + "-".join(converted_files[original_path].split("\\")[4:])
-                dc_description_filename = "Access version | Preservation version"
-        
-        dc_type = "Office Documents"
-        
-        # identify restricted files
-        if "restricted" in name:
-            dc_rights_access = "[ER Restricted until July 1, 2030]"
-            dc_date_open = "2030-07-01"
+    dc_date_issued = "2015"
+    
+    dc_date_created = "2015"
+    
+    if not information_package.get("unitdate", ""):
+        dc_coverage_temporal = "undated"
+    
+    else:
+        if "-" in information_package.get("unitdate", "") or information_package.get("unitdate", "") == "undated" or information_package.get("unitdate", "") == "1989":
+            continue
+        elif information_package.get("unitdate", "") == "6/89":
+            dc_coverage_temporal = "1989-06"
         else:
-            dc_rights_access = "Collection is open for research."
-            # today's date?
-            dc_date_open = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d")
-        
-        dc_rights_copyright = "Copyright has been transferred to the Regents of the University of Michigan."
-        
-        # i think this is right.
-        dc_language_iso = "en_US"
-        
-        with open("deepBlue_9811_0003-TEMP.csv", mode="ab") as metadata_csv:
-            writer = csv.writer(metadata_csv)
-            writer.writerow([
-                identifier_other, 
-                dc_title, 
-                dc_description_abstract, 
-                dc_contributor_author, 
-                dc_contributor_other, 
-                dc_date_issued, 
-                dc_date_created, 
-                dc_coverage_temporal, 
-                dc_title_filename, 
-                dc_description_filename, 
-                dc_type, 
-                dc_rights_access, 
-                dc_date_open, 
-                dc_rights_copyright, 
-                dc_language_iso])
+            try:
+                dc_coverage_temporal = datetime.datetime.strptime(information_package.get("unitdate", ""), "%m/%d/%y")
+            # maybe this is the best thing to do? can't tell how these are typos.
+            except:
+                dc_coverage_temporal = datetime.datetime.strptime(information_package.get("unitdate", ""), "%d/%m/%y")
+                dc_coverage_temporal = dc_coverage_temporal.strftime("%Y-%m-%d")
+                
+    if information_package.get("original", "") != "n/a" and information_package.get("preservation", "") == "n/a":
+        dc_title_filename = information_package.get("original", "")
+        dc_description_filename = ""
+        location = information_package.get("original_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "")
+
+    if information_package.get("original", "") != "n/a" and information_package.get("preservation", "") != "n/a" and information_package.get("autopro", "") == "n/a": 
+        dc_title_filename = information_package.get("original", "") + " | " + information_package.get("preservation", "")
+        dc_description_filename = "Original version | Preservation version"
+        location = information_package.get("original_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "") + " | " + information_package.get("preservation_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "")
+
+    if information_package.get("original", "") != "n/a" and information_package.get("preservation", "") != "n/a" and information_package.get("autopro", "") != "n/a": 
+        dc_title_filename = information_package.get("original", "") + " | " + information_package.get("preservation", "") + " | " + information_package.get("autopro", "")
+        dc_description_filename = "Original version | Preservation version 1 | Preservation version 2"
+        location = information_package.get("original_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "") + " | " + information_package.get("preservation_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "") + " | " + information_package.get("autopro_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "")
+
+    if information_package.get("original", "") == "n/a" and information_package.get("preservation", "") != "n/a" and information_package.get("autopro", "") == "n/a":
+        dc_title_filename = information_package.get("preservation", "")
+        dc_description_filename = "Preservation version"
+        location = information_package.get("preservation_location", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "")
+
+    if information_package.get("original", "") == "n/a" and information_package.get("preservation", "") != "n/a" and information_package.get("autopro", "") != "n/a":
+        dc_title_filename = information_package.get("preservation", "") + " | " + information_package.get("autopro", "")
+        dc_description_filename = "Preservation version 1 | Preservation version 2"
+        location = information_package.get("preservation", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "") + " | " + information_package.get("autopro", "").replace("C:\Users\eckardm\work-stuff\duderstadt", "")
+
+    dc_type = ""
+    
+    if information_package.get("accessrestrict") == True:
+        dc_rights_access = "[ER Restricted until July 1, 2030]"
+        dc_date_open = "2030-07-01"
+    else:
+        dc_rights_access = "Collection is open for research."
+        dc_date_open = "2015"
+
+    dc_rights_copyright = "Copyright has been transferred to the Regents of the University of Michigan."
+    
+    dc_language_iso = ""
+
+    with open("deepBlue_9811_0001.csv", mode="ab") as metadata_csv:
+        writer = csv.writer(metadata_csv)
+        writer.writerow([
+            identifier_other, 
+            dc_title, 
+            dc_description_abstract, 
+            dc_contributor_author, 
+            dc_contributor_other, 
+            dc_date_issued, 
+            dc_date_created, 
+            dc_coverage_temporal, 
+            dc_title_filename, 
+            location,
+            dc_description_filename, 
+            dc_type, 
+            dc_rights_access, 
+            dc_date_open, 
+            dc_rights_copyright, 
+            dc_language_iso])
