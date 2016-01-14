64a987a0bea99b6325a2720190448c06f58c5f48
Max Eckard <eckardm@umich.edu>
2016-01-14 11:52:03 -0500

    back after the holiday break

--- data-wiper/data-wiper.bat
+++ data-wiper/data-wiper.bat
@@ -6,4 +6,7 @@ REM -p passes	Specifies number of overwrite passes (default is 1).
 REM -q	Don't print errors (Quiet).
 REM -s or -r	Recurse subdirectories.
 REM -z	Zero free space (good for virtual disk optimization).
-sdelete -p 7 -s -a "path\to\file"
+"C:\Users\eckardm\work-stuff\data-wiper\SDelete\sdelete.exe" -p 7 -s -a "R:\Dark Archive\Collections\9979\0014"
+"C:\Users\eckardm\work-stuff\data-wiper\SDelete\sdelete.exe" -p 7 -s -a "R:\Content Backup\dark-archive-backup\9979\0014"
+"C:\Users\eckardm\work-stuff\data-wiper\SDelete\sdelete.exe" -p 7 -s -a "V:\9979\0014"
+"C:\Users\eckardm\work-stuff\data-wiper\SDelete\sdelete.exe" -p 7 -s -a "Y:\dark-archive-backup\9979\0014"
