f960b0f7bbabb110ec303a4a47ad54fddfbdba8b
Max Eckard <eckardm@umich.edu>
2016-01-29 17:19:18 -0500

    transcript, i think

--- umsarc/make-oral-history-interviews.py
+++ umsarc/make-oral-history-interviews.py
@@ -1,17 +1,22 @@
 import os
 from bs4 import BeautifulSoup
 import re
+from lxml import etree
+
+print "Getting metadata..."
 
 oral_history_interviews = [name for name in os.listdir("substance.abuse.history") if "oral_history_interviews&findAll=true&mode=single&recordID=" in name and "&nextMode=list" not in name]
 
-metadata_list = []
+metadata = []
 
 for name in oral_history_interviews:
 
-    with open(os.path.join("substance.abuse.history", name), mode="r") as input:
-        soup = BeautifulSoup(input, "lxml")
+    with open(os.path.join("substance.abuse.history", name), mode="r") as oral_history_interview:
+        soup = BeautifulSoup(oral_history_interview, "lxml")
         
         metadata_dict = {}
+        
+        metadata_dict["name"] = name
         
         interviewee = soup.find(text="Interviewee: ").parent.parent.find_next_siblings("td")[0].text.strip()
         if interviewee.endswith("Retired, NIDA Intramural Research Program, Baltimore, MD"):
@@ -22,10 +27,10 @@ for name in oral_history_interviews:
         
         biographical_note = soup.find(text=re.compile("^Biographical"))
         if biographical_note:
-            biographical_note = biographical_note.parent.parent.find_next_siblings("td")[0].encode("utf-8")
+            biographical_note = str(biographical_note.parent.parent.find_next_siblings("td")[0].encode("utf-8")).replace("<td>", "").replace("</td>", "")
             metadata_dict["biographical_note"] = biographical_note
         
-        interviewer = soup.find(text="Interviewer(s): ").parent.parent.find_next_siblings("td")[0].text.strip()
+        interviewer = soup.find(text="Interviewer(s): ").parent.parent.find_next_siblings("td")[0].text
         if len(interviewer) > 0:
             metadata_dict["interviewer"] = interviewer        
             
@@ -34,8 +39,28 @@ for name in oral_history_interviews:
             date = date.parent.parent.find_next_siblings("td")[0].text.strip()
             metadata_dict["date"] = date
         
-        suggested_citation = ""
+        suggested_citation = soup.find(text=re.compile("^Suggested"))
+        if suggested_citation:
+            suggested_citation = suggested_citation.parent.parent.find_next_siblings("td")[0].text
+            metadata_dict["suggested_citation"] = suggested_citation
         
-        keywords = ""
+        keywords = soup.find(text="Keywords:")
+        if keywords:
+            keywords = keywords.parent.parent.find_next_siblings("td")[0].text
+            metadata_dict["keywords"] = keywords
+            
+        metadata.append(metadata_dict)
         
-        metadata_list.append(metadata_dict)
+print "Getting transcripts..."
+
+for metadatum in metadata:
+
+    with open(os.path.join("substance.abuse.history", metadatum["name"].replace("oral_history_interviews&findAll=true&mode=single&recordID=", "manage_interviews&mode=single&recordID=")), mode="r") as oral_history_interview:
+        soup = BeautifulSoup(oral_history_interview, "lxml")
+        
+        possible_transcripts = soup.find_all("textarea")
+        for possible_transcript in possible_transcripts:
+            if possible_transcript["name"].startswith("Transcript"):
+                metadatum["transcript"] = possible_transcript.text.encode("utf-8")
+                
+print "Making PDFs..."
