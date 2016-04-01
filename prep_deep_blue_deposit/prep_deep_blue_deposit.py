bff24dd524060e9562366a801279608177e6853e
Max Eckard <eckardm@umich.edu>
2016-04-01 16:06:17 -0400

    saf: archive_directory

--- prep_deep_blue_deposit/prep_deep_blue_deposit.py
+++ prep_deep_blue_deposit/prep_deep_blue_deposit.py
@@ -1,10 +1,12 @@
 import os
+from openpyxl import load_workbook
 
 deposit_id = raw_input("Deposit ID: ")
 
 source_directory = os.path.join("X:\deepblue", deposit_id)
 target_directory = "S:\MLibrary\DeepBlue"
 
+'''
 # make working copy
 os.putenv("SOURCE_DIRECTORY", source_directory)
 
@@ -12,3 +14,10 @@ os.makedirs(deposit_id)
 os.putenv("TARGET_DIRECTORY", "C:\Users\eckardm\work-stuff\prep_deep_blue_deposit")
 
 os.system("copy_with_teracopy.bat")
+
+# make simple archive format
+# make archive_directory
+os.rename(deposit_id, "archive_directory")'''
+
+    
+    
