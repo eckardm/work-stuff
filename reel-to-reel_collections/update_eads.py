b4117ba73b37e71ee88a1c7efbb976327f49a54c
Max Eckard <eckardm@umich.edu>
2016-07-13 11:40:48 -0400

    updates after dallas' help

--- reel-to-reel_collections/update_eads.py
+++ reel-to-reel_collections/update_eads.py
@@ -1,64 +1,44 @@
+# dallas solved it for me... https://gist.github.com/djpillen/a580862bfdae4ca202b1840b9a64a65e
+
 import os
 from lxml import etree
-
-
-path_to_eads = os.path.join("C:/", "Users", "eckardm", "vandura-test", "Real_Masters_all")
-eads = [ead for ead in os.listdir(path_to_eads) if ead.endswith(".xml")]
-
-collno_ead = {}
-
-for ead in eads:
-    tree = etree.parse(os.path.join(path_to_eads, ead))
-    
-    eadid = tree.xpath("/ead/eadheader/eadid")[0]
-    collno = eadid.text.strip().split("-")[2]
-    
-    collno_ead[collno] = ead
  
+collitemno_handle = {}
  
-mapfiles = [mapfile for mapfile in os.listdir("reelmapfiles")]
- 
-collnos = []
-collno_mapfile = {}
-    
-for mapfile in mapfiles:
-    with open(os.path.join("reelmapfiles", mapfile)) as file:
-        collno = file.readlines()[0].strip().split("-")[0]
-        
-        collnos.append(collno)
-        
-        collno_mapfile[collno] = mapfile
-        
-
-for collno in collnos:
-    print collno_ead.get(collno)
-
-    mapfile_unitids = []
-    mapfile_unitid_handle = {}
-    
-    with open(os.path.join("reelmapfiles", collno_mapfile.get(collno, ""))) as file:
+for filename in os.listdir("reelmapfiles"):
+    with open(os.path.join("reelmapfiles", filename), mode="r") as file:
         for line in file.readlines():
-            mapfile_unitid = line.strip().split(" ")[0]
+            collitemno = line.strip().split(" ")[0]
             handle = line.strip().split(" ")[1]
             
-            mapfile_unitids.append(mapfile_unitid)
-            
-            mapfile_unitid_handle[mapfile_unitid] = handle
+            collitemno_handle[collitemno] = handle
+
+path_to_eads = os.path.join("C:/", "Users", "eckardm", "vandura-test", "Real_Masters_all")
+
+for filename in os.listdir(path_to_eads):
+    tree = etree.parse(os.path.join(path_to_eads, filename))
+    unitids = tree.xpath("//unitid")
     
-    for mapfile_unitid in mapfile_unitids:
-        tree = etree.parse(os.path.join(path_to_eads, collno_ead.get(collno, "")))
-        
-        ead_unitids = tree.xpath("//unitid")
-        
-        for ead_unitid in ead_unitids:
-            if ead_unitid.text == "[" + mapfile_unitid + "]":
-                print mapfile_unitid.text
-                print mapfile_unitid_handle.get(mapfile_unitid, "")
-                
-                parent_component = ead_unitid.getparent().getparent().getparent()
+    for unitid in unitids:
+        if "sr" in unitid.text.lower():
+            digfilecalc = unitid.text.strip("[] ")
+            
+            href = ""
+            
+            while not href and digfilecalc is not None:
+                if digfilecalc in collitemno_handle:
+                    href = "http://hdl.handle.net/" + collitemno_handle.get(digfilecalc)
+                    del collitemno_handle[digfilecalc]
+                else:
+                    digfilecalc = "-".join(digfilecalc.split("-")[:-1])
+                 
+            if href:
+                print digfilecalc
+            
+                parent_component = unitid.getparent().getparent().getparent()
                 
                 dao = etree.Element("dao")
-                dao.attrib["href"] = "http://hdl.handle.net/" + mapfile_unitid_handle.get(mapfile_unitid, "")
+                dao.attrib["href"] = href
                 dao.attrib["show"] = "new"
                 dao.attrib["actuate"] = "onrequest"
                 
@@ -69,8 +49,13 @@ for collno in collnos:
                 p.text = "[view item]"
                 daodesc.append(p)
                 
-                parent_component.find("did").append(dao)
-                
-                # with open(os.path.join(path_to_eads, collno_ead.get(collno, ""))), mode="w") as ead_out:
-                    # ead_out.write(etree.tostring(tree, encoding='utf-8', xml_declaration=True))
+                if not parent_component.find("did").find("dao"):
+                    parent_component.append(dao)
+        
+                    # with open(os.path.join(path_to_eads, filename, mode="w") as ead_out:
+                        # ead_out.write(etree.tostring(tree, encoding="utf-8", xml_declaration=True))
+
+if collitemno_handle:
+    for collitemno in collitemno_handle:
+        print collitemno, handle
                 
