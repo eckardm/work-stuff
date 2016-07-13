a41c0f6cdd9580bcab4f3c767204b9fbda62ca94
Max Eckard <eckardm@umich.edu>
2016-07-13 09:46:49 -0400

    i think this will do it!

--- reel-to-reel_collections/update_eads.py
+++ reel-to-reel_collections/update_eads.py
@@ -34,12 +34,16 @@ for collno in collnos:
     print collno_ead.get(collno)
 
     mapfile_unitids = []
+    mapfile_unitid_handle = {}
     
     with open(os.path.join("reelmapfiles", collno_mapfile.get(collno, ""))) as file:
         for line in file.readlines():
             mapfile_unitid = line.strip().split(" ")[0]
+            handle = line.strip().split(" ")[1]
             
             mapfile_unitids.append(mapfile_unitid)
+            
+            mapfile_unitid_handle[mapfile_unitid] = handle
     
     for mapfile_unitid in mapfile_unitids:
         tree = etree.parse(os.path.join(path_to_eads, collno_ead.get(collno, "")))
@@ -48,4 +52,25 @@ for collno in collnos:
         
         for ead_unitid in ead_unitids:
             if ead_unitid.text == "[" + mapfile_unitid + "]":
-                print ead_unitid.text
+                print mapfile_unitid.text
+                print mapfile_unitid_handle.get(mapfile_unitid, "")
+                
+                parent_component = ead_unitid.getparent().getparent().getparent()
+                
+                dao = etree.Element("dao")
+                dao.attrib["href"] = "http://hdl.handle.net/" + mapfile_unitid_handle.get(mapfile_unitid, "")
+                dao.attrib["show"] = "new"
+                dao.attrib["actuate"] = "onrequest"
+                
+                daodesc = etree.Element("daodesc")
+                dao.append(daodesc)
+                
+                p = etree.Element("p")
+                p.text = "[view item]"
+                daodesc.append(p)
+                
+                parent_component.find("did").append(dao)
+                
+                # with open(os.path.join(path_to_eads, collno_ead.get(collno, ""))), mode="w") as ead_out:
+                    # ead_out.write(etree.tostring(tree, encoding='utf-8', xml_declaration=True))
+                
